b438a4d8baa5bc50457d5ebcd0ffcbc2f8e2e2c6
Yuanfei Zhu <yuanfei@satori.works>
2020-01-18 01:16:49 +0800

    Wrapping event stream using PubSub

--- api/main.py
+++ api/main.py
@@ -1,5 +1,55 @@
 from wotapi.services.camera import CameraService
+from wotapi.utils import logger
+import asyncio
+import paco
 
 
-cs = CameraService()
-print(cs.get_info())
+from aio_pubsub.backends.memory import MemoryPubSub
+
+
+async def run_hub():
+    """
+    Testing PubSub
+    """
+    pubsub = MemoryPubSub()
+    # Create subscriber
+    subscriber = await pubsub.subscribe("a_chan")
+
+    # Push message
+    await pubsub.publish("a_chan", "hello world!")
+    await pubsub.publish("a_chan", "hello universe!")
+    await pubsub.publish("a_chan", "1")
+
+    # And listening channel
+    try:
+        async for message in subscriber:
+            print(message, flush=True)
+    except KeyboardInterrupt:
+        print("Finish listening")
+
+
+async def listen(observable, name):
+    """
+    Handler for incoming events
+    """
+    async for item in observable:
+        logger.debug(f"{name}: get item {item.keys()!s}")
+        if name == 'intensity_stream':
+            logger.debug(item['stats'])
+
+
+async def main():
+    cs = CameraService()
+    info = cs.get_info()
+    logger.debug(f"Camera info: {info!s}")
+    # Connect w/ the event stream
+    await cs.init_subscribers()
+    # Kick off the subscribers
+    await paco.gather(
+        cs.connect(),
+        listen(cs.image_stream, "image_stream"),
+        listen(cs.intensity_stream, "intensity_stream"),
+    )
+
+
+paco.run(main())
--- api/wotapi/services/camera.py
+++ api/wotapi/services/camera.py
@@ -4,6 +4,22 @@ from numpngw import write_png
 import time
 import rpyc
 import numpy as np
+from aioify import aioify
+import asyncio
+from aio_pubsub.backends.memory import MemoryPubSub, MemorySubscriber
+import paco
+
+
+class AMemorySubscriber(MemorySubscriber):
+    async def __anext__(self):
+        return await self.messages.get()
+
+
+class AMemoryPubSub(MemoryPubSub):
+    async def subscribe(self, channel: str):
+        subscriber = AMemorySubscriber(self.queue_factory)
+        self.subscribers[channel].add(subscriber)
+        return subscriber
 
 
 class CameraQueueManager(BaseManager):
@@ -45,5 +61,37 @@ class CameraService:
         self.status_queue = self.queue_mgr.status_queue()
         self.cmd_queue = self.queue_mgr.cmd_queue()
 
+        # Hub for PubSub
+        self.hub = AMemoryPubSub(asyncio.Queue)
+
+        # TODO: default to NullSubscriber
+        self.image_stream = None
+        self.intensity_stream = None
+
     def get_info(self):
         return self.rpc.getCamera()
+
+
+    async def connect(self):
+        # Start receiving item from RPC calls
+        while True:
+            item = await paco.wraps(self.status_queue.get)()
+
+            # Distribute item according to its topic
+            if "CIMG" in item or "TIMG" in item:
+                await self.hub.publish("image", item)
+            elif "INT" in item:
+                await self.hub.publish("intensity", {
+                    'samples': item['INT'],
+                    'stats': {
+                        'fps': item['ISTAT'][0],
+                        'lptc': item['ISTAT'][1]
+                    }
+                })
+
+            await asyncio.sleep(0.5)
+
+    async def init_subscribers(self):
+        self.image_stream = await self.hub.subscribe("image")
+        self.intensity_stream = await self.hub.subscribe("intensity")
+
