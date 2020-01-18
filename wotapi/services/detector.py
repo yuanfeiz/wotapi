e622ba6923c7075534455f62f232fbbf38c52965
Yuanfei Zhu <yuanfei@satori.works>
2020-01-18 19:33:54 +0800

    Fixed demo

--- wotapi/services/detector.py
+++ wotapi/services/detector.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import logging
 import rpyc
+import time
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -14,14 +15,14 @@ class DetectorService:
 
     def __init__(self, ip="localhost", port=51237):
         config = {"allow_pickle": True}
-        self.rpc = rpyc.connect(ip, port, config).root
+        self.rpc = rpyc.connect(ip, port, config=config).root
 
     def get_info(self):
         return self.rpc.getCamera()
 
 
 if __name__ == "__main__":
-    sample_path = Path("/Users/yuanfei/Projects/siu/wot/wot-app/data/triggered_img.png")
+    sample_path = Path("/projects/siu/wot/wotapi/data/")
 
     ds = DetectorService()
 
@@ -29,6 +30,6 @@ if __name__ == "__main__":
     ds.rpc.startDetector(str(sample_path), True)
 
     while True:
-        logging.info(f"{dc.rpc.getPos()=}")
+        logging.info(f"{ds.rpc.getPos()=}")
         logging.info(f"{ds.rpc.getResults()=}")
         time.sleep(0.5)
