75c25b58fbe65af286075814fcb386562d8fbfa0
Yuanfei Zhu <abovemoon@outlook.com>
2020-02-08 14:30:13 +0800

    Drop dependencies for paco

--- wotapi/views/automode.py
+++ wotapi/views/automode.py
@@ -29,11 +29,11 @@ async def start_auto_mode_task(request):
 
     asyncio.create_task(
         # Cancel progress report when task is done(reflecting by t)
-        paco.race([
+        asyncio.wait({
             notify_done(t),
             notify_updated(tid, scheduler_sub, SchedulerEventParser()),
             notify_updated(tid, worker_sub, RunProgressParser()),
-        ]))
+        }))
 
     return json_response({
         "id": tid,
